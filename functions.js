31c95ead2e4d102c135cfbd92ed6dd2735c6c035
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-07-14 13:43:27 +0100

    add Object.assign()

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 const functions = {
-
+    objAssign: (inputObj) => Object.assign({name: 'Dale Cooper'}, inputObj)
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -1,1 +1,5 @@
 const functions = require('./functions')
+
+test('returns a new object with passed source object added to target object', () => {
+    expect(functions.objAssign({whereabouts: 'unknown'})).toStrictEqual({name: 'Dale Cooper', whereabouts: 'unknown'})
+})
