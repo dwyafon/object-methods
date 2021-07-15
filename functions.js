80daa586bdeda65c53c2d6677cb3ae5a3dc17b5c
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-07-15 14:19:12 +0100

    add Object.create()

--- functions.js
+++ functions.js
@@ -1,5 +1,9 @@
 const functions = {
-    objAssign: (sourceObj) => Object.assign({name: 'Dale Cooper'}, sourceObj)
+    objAssign: (sourceObj) => Object.assign({name: 'Dale Cooper'}, sourceObj),
+    objCreate: (prototypeObj) => {
+        const newObj = Object.create(prototypeObj)
+        return Object.getPrototypeOf(newObj) === prototypeObj
+    },
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,14 @@
 const functions = require('./functions')
 
+// Object.assign()
 test('returns a new object with passed source object added to target object', () => {
-    expect(functions.objAssign({whereabouts: 'unknown'})).toStrictEqual({name: 'Dale Cooper', whereabouts: 'unknown'})
+  expect(functions.objAssign({ whereabouts: 'unknown' })).toStrictEqual({
+    name: 'Dale Cooper',
+    whereabouts: 'unknown',
+  })
+})
+
+// Object.create() and Object.getPrototypeOf()
+test('returns true when the input object is checked as the protoype for the created object', () => {
+  expect(functions.objCreate({})).toStrictEqual(true)
 })
