32a06f980934ab55cf7305cf0943cd2d8729932f
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-07-17 18:43:41 +0100

    add Object.defineProperties()

--- functions.js
+++ functions.js
@@ -1,9 +1,22 @@
 const functions = {
-    objAssign: (sourceObj) => Object.assign({name: 'Dale Cooper'}, sourceObj),
-    objCreate: (prototypeObj) => {
-        const newObj = Object.create(prototypeObj)
-        return Object.getPrototypeOf(newObj) === prototypeObj
-    },
+  objAssign: sourceObj => Object.assign({ name: 'Dale Cooper' }, sourceObj),
+  objCreate: prototypeObj => {
+    const newObj = Object.create(prototypeObj)
+    return Object.getPrototypeOf(newObj) === prototypeObj
+  },
+  objDefineProperties: inputObj => {
+    const modifedObj = Object.defineProperties(inputObj, {
+      name: { value: 'Dale Cooper', writable: true },
+    })
+    return modifedObj.name
+  },
+  objDefineProperties2: inputObj =>
+    Object.defineProperties(inputObj, {
+      choiceOfPie: {
+        value: 'Cherry',
+        writable: true,
+      },
+    }),
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -12,3 +12,18 @@ test('returns a new object with passed source object added to target object', ()
 test('returns true when the prototype of the object created within the function is checked as strictly equal to the passed prototype object', () => {
   expect(functions.objCreate({})).toStrictEqual(true)
 })
+
+// Object.defineProperties()
+test('receives an empty object and returns an object to which one property has been added', () => {
+  expect(functions.objDefineProperties({})).toStrictEqual('Dale Cooper')
+})
+
+test('receives an object and returns an object to which one property has been modifed', () => {
+  expect(functions.objDefineProperties({name: 'Agent Cooper'})).toStrictEqual('Dale Cooper')
+})
+
+test('receives an empty object and returns the same empty object despite modifications to that object', () => {
+  expect(functions.objDefineProperties2({})).toEqual({})
+})
+
+// Object.defineProperty()
